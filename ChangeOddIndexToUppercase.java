4a6e0ec26095439349412b09cfe6f930ab068927
Vijivino <112789812+Vijivino@users.noreply.github.com>
2022-09-05 17:21:43 +0530

    Add files via upload

---
+++ ChangeOddIndexToUppercase.java
@@ -0,0 +1,24 @@
+package week1.day2;
+
+public class ChangeOddIndexToUppercase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String test="changeme";
+		char[] charArray = test.toCharArray();
+		System.out.println("odd index of changeme are");
+	for(int i=0;i<charArray.length;i++)
+	{ 
+	if (i%2!=0)
+	{
+		charArray[i]=Character.toUpperCase(charArray[i]);
+		//String string = charArray.toString();
+		System.out.print(charArray[i]);
+		
+	}
+	else
+		System.out.print(charArray[i]);
+	}
+}
+}
+
---
+++ ReverseEvenIndex.java
@@ -0,0 +1,29 @@
+package week1.day2;
+
+public class ReverseEvenIndex {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String test="I am a software tester";
+		String[] split = test.split(" ");
+		for(int i=0;i<split.length;i++)
+		{
+			if (i%2 == 0)
+			{
+				String str = split[i].toString();
+				char[] charArray = str.toCharArray();
+				for(int j=charArray.length-1;j>=0;j--)
+				{
+					System.out.print(charArray[j]);
+				}
+				System.out.print(" ");
+			}
+			else 
+			{
+				System.out.print(split[i]+ " ");
+			}
+		}
+}
+	
+}
